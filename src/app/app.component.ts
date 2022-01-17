f9512083ce5b3ca481eae5ea7c3c96bc9492ae46
shubham120 <sourabh.shubham120@gmail.com>
2022-01-17 10:33:29 +0530

    prod issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { slideInAnimation } from './app-common/animations/animations';
 import { AuthService } from './app-common/services/auth.service';
 
@@ -12,11 +13,19 @@ export class AppComponent implements OnInit {
   title = 'health-tracker';
   showDefaultTemplate: boolean = false;
 
-  constructor(private authService: AuthService) {
-    console.log(document.hidden);
-  }
+  constructor(private authService: AuthService, private swUpdate: SwUpdate) {}
 
   ngOnInit(): void {
     this.authService.initAuthListener();
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe({
+        next: () => {
+          if (confirm('New version available, reload?')) {
+            window.location.reload();
+          }
+        },
+      });
+    }
   }
 }
